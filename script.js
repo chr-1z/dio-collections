4a991411817eb01da0a891d585929f2e6e22d8bb
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-12-30 14:42:06 -0300

    Atividade Map & Set

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
+
+CRIE UMA FUNÇÃO QUE RETORNA O NOME DOS MEMBROS DE UM MAP QUE TEM O PAPEL ADMIN NO SISTEMA.
+
+1. Crie uma função getAdmin que recebe um Map.
+2. Crie um Map e popule-o com nomes de usuários e seus papeis no sistema (Ex: 'Luiz' => 'Admin').
+3. Dentro de getAdmins, utilize o loop for...of para retornar uma lista com os nomes dos usuários que são administradores.
+
+*/
+
 function getAdmins(map) {
     let admins = [];
     for([key, value] of map){
@@ -26,6 +36,23 @@ usuarios.set('Jorge', 'User');
 usuarios.set('Júlia', 'Admin');
 
 console.log(getAdmins(usuarios));
+// console.log(getUsers(usuarios));
 
-console.log(getUsers(usuarios));
+/*
+
+DADO O ARRAY [30, 30, 40, 5, 223, 2049, 3034, 5]
+
+Retorne outro array apenas com valores únicos.
+
+*/
+
+const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+function valoresUnicos(array) {
+    const mySet = new Set(array);
+
+    return [...mySet];
+}
+
+console.log(valoresUnicos(meuArray));
 
